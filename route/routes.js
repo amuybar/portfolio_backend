83a2d36fe63632b610ee9cf41b9af5050949cca8
Barrack Amuyunzu <amuyunzubq@gmail.com>
2024-05-02 07:10:25 +0300

    Updated the mailer function to include a structured message

--- route/routes.js
+++ route/routes.js
@@ -15,8 +15,18 @@ router.post('/message', async (req, res) => {
       from: 'barryreact@gmail.com', // Replace with your email
       to: email,
       subject: 'Portfolio Message Confirmation',
-      text: `Thank you ${name} for your message!`,
+      text: `Thank you ${name} for your message!\n\n
+             Hello ${name},\n\n
+             Thank you for reaching out regarding my web development services. I'm excited to assist you in bringing your vision to life!\n\n
+             I specialize in creating stunning and functional websites tailored to meet your unique needs. Whether you're looking to establish your online presence, revamp an existing website, or develop a custom web application, I've got you covered.\n\n
+             With a focus on quality, creativity, and innovation, I ensure that every project is delivered with precision and attention to detail. My goal is not only to meet your expectations but to exceed them, providing you with a website that not only looks great but also drives results.\n\n
+             Your satisfaction is my priority, and I'm committed to delivering projects on time and within budget. You can rely on me to communicate effectively, address your concerns promptly, and provide ongoing support even after the project is complete.\n\n
+             Feel free to reach out to me via WhatsApp at 0742812483 to discuss your project further or if you have any questions. I'm here to help!\n\n
+             Looking forward to the opportunity to work together.\n\n
+             Best regards,\n
+             Barrack Amuyunzu`,
     };
+    
     await sendEmail(emailOptions);
 
     res.json({ message: 'Message sent successfully!' });
@@ -36,7 +46,7 @@ router.post('/request', async (req, res) => {
       from: 'barryreact@gmail.com', // Replace with your email
       to: 'odaribq@gmail.com',  // Replace with recipient email
       subject: 'New Service Request on Portfolio',
-      text: `New request from: ${fullName} - ${email}`,
+      text: `New request from: ${fullName}\n\n\n ${email}\n\n\n ${tel}`,
     };
     await sendEmail(emailOptions);
 
